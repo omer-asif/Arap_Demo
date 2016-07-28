2ecf3f181b13be0e59d4adcd4e58ec6a7efa870b
omer-asif <omer-asif@users.noreply.github.com>
2016-07-28 18:09:36 +0200

    Update main.cpp

--- my_example/main.cpp
+++ my_example/main.cpp
@@ -1,9 +1,3 @@
-#include <CGAL/Simple_cartesian.h>
-#include <CGAL/Polyhedron_3.h>
-#include <CGAL/Polyhedron_items_with_id_3.h>
-#include <CGAL/IO/Polyhedron_iostream.h>
-#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
-#include <CGAL/boost/graph/properties_Polyhedron_3.h>
 
 #include "Arap_mesh_deformation.h"
 
@@ -23,48 +17,16 @@
 
 #define DEFORMED_MESH_PATH "deformed_mesh.off"
 
+typedef Arap_mesh_deformation mesh_deformation;
 
-typedef CGAL::Simple_cartesian<double>                                   Kernel;
-typedef CGAL::Polyhedron_3<Kernel, CGAL::Polyhedron_items_with_id_3> Polyhedron;
-typedef Arap_mesh_deformation<Polyhedron> mesh_deformation;
-typedef mesh_deformation::vertex_descriptor vertex_descriptor;
-typedef mesh_deformation::vertex_iterator vertex_iterator;
-Polyhedron mesh;
 mesh_deformation *deform_mesh = NULL;
-vertex_iterator vb, ve;
+Eigen::MatrixXd V, V_temp, C;
+Eigen::MatrixXi F, F_temp;
+igl::viewer::Viewer viewer;
+int last_vid = -1;
+bool flag = false;
 
-
-
-class Timer
-{
-public:
-	Timer() : beg_(clock_::now()) {}
-	void reset() { beg_ = clock_::now(); }
-	double elapsed() const {
-		return std::chrono::duration_cast<second_>
-			(clock_::now() - beg_).count();
-	}
-
-private:
-	typedef std::chrono::high_resolution_clock clock_;
-	typedef std::chrono::duration<double, std::ratio<1> > second_;
-	std::chrono::time_point<clock_> beg_;
-};
-
-
-// CGAL related mesh setup
-bool setup_mesh_for_deformation(const char* filename) {
-
-	mesh.clear();
-	std::ifstream input(filename);
-	
-	if (!input || !(input >> mesh) || mesh.empty()) {
-		std::cerr << "Cannot open:  " << filename << std::endl;
-		return false;
-	}
-
-	// Init the indices of the halfedges and the vertices.
-	set_halfedgeds_items_id(mesh);
+bool setup_mesh_for_deformation(const char* filename, Eigen::MatrixXd &V, Eigen::MatrixXi &F) {
 
 	// Create a deformation object
 	if (deform_mesh != NULL) {
@@ -72,34 +34,67 @@ bool setup_mesh_for_deformation(const char* filename) {
 		deform_mesh = NULL;
 	}
 
-	deform_mesh = new mesh_deformation(mesh);
-	boost::tie(vb, ve) = vertices(mesh);
-	deform_mesh->insert_roi_vertices(vb, ve); // use whole mesh as ROI
+	deform_mesh = new mesh_deformation(V, F);
+	deform_mesh->insert_roi_vertices(0, V.rows()-1); // use whole mesh as ROI
 	return true;
 }
 
+void update_ui()
+{
+	viewer.data.set_vertices(V_temp);
+	viewer.data.set_colors(C);
+}
 
+void perform_deformation()
+{
+	Timer tmr;
+	flag = true;
+	std::vector<std::thread> lst;
+	for (int l = 0; l < 20; l++)
+	{
+		deform_mesh->deform(1, 0.0);
+		if(l%2 == 0 && l!=0)
+			lst.push_back(std::thread(update_ui));
+	}
+	igl::writeOFF(DEFORMED_MESH_PATH, V, F);
+
+	// Reset everything to start again using the deformed mesh
+	last_vid = -1;
+	V = V_temp;
+	viewer.data.set_vertices(V);
+	viewer.data.set_colors(C);
+	deform_mesh->reset();
+	double t = tmr.elapsed();
+	for (auto&th : lst)
+	{
+		th.join();
+	}
+	std::cout << "Total Elapsed Time: " << t << std::endl; // Time Elapsed
+	flag = false;
+}
 
 
 int main(int argc, char *argv[])
 {
   float original_z = 0.0f;
   bool clicked_outside_mesh = true;
-  int last_vid = -1, roi_radius=3;
+  
+  std::thread *deformation_thread = NULL;
+  std::map<int, Eigen::Vector3d> id_point_map;
 
-  std::map<int, mesh_deformation::Point> id_point_map;
+  
 
-  Eigen::MatrixXd V, C;
-  Eigen::MatrixXi F;
-  // Load a mesh in OFF format
   igl::readOFF(ORIGINAL_MESH_PATH, V, F);
+
+  V_temp = V;
+  F_temp = F;
 
   // Initialize with white color
   C= Eigen::MatrixXd::Constant(V.rows(),3,1); // Initialize Color matrix with size of Vertices, for per vertex coloring, white by default
-  igl::viewer::Viewer viewer;
+  
 
   //Polyhedron mesh;
-  if (!setup_mesh_for_deformation(ORIGINAL_MESH_PATH)) // Setup CGAL related mesh data structures
+  if (!setup_mesh_for_deformation(ORIGINAL_MESH_PATH, V_temp, F_temp))
   {
 	  return 1; // error occured, exit the program
   }
@@ -108,6 +103,9 @@ int main(int argc, char *argv[])
   viewer.callback_mouse_up =
 	  [&](igl::viewer::Viewer& viewer, int btn, int)->bool 
   {
+	  if (flag) {
+		  return false;
+	  }
 	  if (last_vid != -1 && !clicked_outside_mesh)
 	  {
 		  GLint x = viewer.current_mouse_x; 
@@ -121,31 +119,17 @@ int main(int argc, char *argv[])
 		  
 		  // Set target positions of control vertices
 
-		  id_point_map[last_vid] = mesh_deformation::Point(ss.x(), ss.y(), ss.z());
-		  vertex_descriptor vd;
+		  id_point_map[last_vid] = Eigen::Vector3d(ss.x(), ss.y(), ss.z());
 		  
 		 // Have to set target positions for all ctrls coz during reset we lost these positions 
-		 for (std::map<int, mesh_deformation::Point>::iterator ii = id_point_map.begin(); ii != id_point_map.end(); ++ii)
+		 for (std::map<int, Eigen::Vector3d>::iterator ii = id_point_map.begin(); ii != id_point_map.end(); ++ii)
 		  {
-			 vd = *CGAL::cpp11::next(vb, ii->first);
-			 deform_mesh->set_target_position(vd,ii->second);
+			 deform_mesh->set_target_position(ii->first,ii->second);
 		  }
 		 
 		  Timer tmr;
-		  deform_mesh->deform(20, 0.0);
-		  
-		  std::ofstream output(DEFORMED_MESH_PATH);
-		  output << mesh;
-		  output.close();
-		  // Reset everything to start again using the deformed mesh
-		  last_vid = -1;
-		  igl::readOFF(DEFORMED_MESH_PATH, V, F); // Load the deformed mesh as current mesh
-		  viewer.data.set_mesh(V, F);
-		  viewer.data.set_colors(C);
-		  deform_mesh->reset();
-		  double t = tmr.elapsed();
-		  
-		  std::cout << "Total Elapsed Time: " << t << std::endl; // Time Elapsed
+		  deformation_thread = new std::thread(perform_deformation);
+
 		  return true;
 	  }
 	  return false;
@@ -155,7 +139,12 @@ int main(int argc, char *argv[])
   viewer.callback_mouse_down = 
     [&](igl::viewer::Viewer& viewer, int btn, int)->bool
   {
-    int face_id;
+	
+	if (flag) {
+		return false;
+	}
+
+	int face_id;
     Eigen::Vector3f bc;
 	
     double x = viewer.current_mouse_x;
@@ -178,12 +167,10 @@ int main(int argc, char *argv[])
 		  int i;
 		  bc.maxCoeff(&i);
 		  last_vid = F(face_id, i); // retrieve the vertex id clicked by using the retrieved face_id
-		  
-		  vertex_descriptor control_vertex;
-		  mesh_deformation::Point point_1;
+		  Eigen::Vector3d point_1;
 
-		  control_vertex = *CGAL::cpp11::next(vb, last_vid);
-		  deform_mesh->insert_control_vertex(control_vertex, point_1);
+		  //control_vertex = *CGAL::cpp11::next(vb, last_vid);
+		  deform_mesh->insert_control_vertex(last_vid, point_1);
 
 		  C.row(last_vid) = Eigen::RowVector3d(1, 0, 0);
 		  viewer.data.set_colors(C);
@@ -203,19 +190,10 @@ int main(int argc, char *argv[])
     return false;
   };
 
-  
-  viewer.callback_init = [&](igl::viewer::Viewer& viewer)
-  {
-	  viewer.ngui->addGroup("Mesh Deformation");
-	  viewer.ngui->addVariable("ROI radius", roi_radius); // you can change this from UI to change how much of neighbouring region should be affected.
-	  // call to generate menu
-	  viewer.screen->performLayout();
-	  return false;
-  };
-
   // Show mesh
   viewer.data.set_mesh(V, F);
   viewer.data.set_colors(C);
   viewer.core.show_lines = false;
   viewer.launch();
+  
 }
