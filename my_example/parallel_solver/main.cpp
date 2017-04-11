5e5bee860fe99eef3eaa9dec478271f62e957beb
omer-asif <omer-asif@users.noreply.github.com>
2017-04-11 17:54:31 +0200

    Create main.cpp

---
+++ my_example/parallel_solver/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <stdio.h>
+#include <malloc.h>
+
+extern "C" void solve();
+
+int main( int argc, char** argv){
+
+	solve();
+	int jj = 0;
+	std::cin >> jj;
+}
